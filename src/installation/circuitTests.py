541414dc9336be32a3f0f1e6c68f05f247d438c3
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2023-02-23 12:10:32 -0300

    Setup unit testing

---
+++ src/installation/circuitTests.py
@@ -0,0 +1,25 @@
+# 
+# NBR 5410 Calculator
+# 
+# 
+# Author: Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
+
+
+
+from unittest import TestCase
+
+from .circuit import Circuit
+
+
+
+class CircuitTests( TestCase ):
+	'''
+	Tests for `Circuit` class.
+	'''
+	
+	def testExample( self ):
+		'''
+		.
+		'''
+		
+		self.assertIs( True, True )
