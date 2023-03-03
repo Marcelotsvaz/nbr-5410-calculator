337305b006c57e5c0d64d96acd505a194941a080
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2023-03-03 07:26:29 -0300

    Don't record line numbers in translations

--- share/translations/app_en_US.ts
+++ share/translations/app_en_US.ts
@@ -4,82 +4,66 @@
 <context>
     <name>CircuitsModel</name>
     <message>
-        <location filename="../../src/circuitsTab.py" line="83"/>
         <source>Name</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="84"/>
         <source>Power</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="85"/>
         <source>Load Type</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="86"/>
         <source>Voltage</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="87"/>
         <source>Phases</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="88"/>
         <source>Grouping</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="89"/>
         <source>Temperature</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="90"/>
         <source>Ref. Method</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="91"/>
         <source>Configuration</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="92"/>
         <source>Wire Type</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="93"/>
         <source>Length</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="94"/>
         <source>Current</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="95"/>
         <source>Breaker</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="96"/>
         <source>Wire Capacity</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="97"/>
         <source>Wire Section</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="201"/>
         <source>New Circuit</source>
         <translation></translation>
     </message>
@@ -87,97 +71,78 @@
 <context>
     <name>mainWindow</name>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="15"/>
         <source>NBR 5410 Calculator</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="26"/>
         <source>Panel</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="31"/>
         <source>Circuits</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="81"/>
         <source>New circuit</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="92"/>
         <source>Delete circuit</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="104"/>
         <source>Conduits</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="123"/>
         <source>&amp;File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="135"/>
         <source>&amp;Help</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="148"/>
         <source>&amp;New</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="160"/>
         <source>&amp;Open...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="172"/>
         <source>&amp;Save</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="184"/>
         <source>Save &amp;As...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="196"/>
         <source>&amp;Quit</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="208"/>
         <source>&amp;About</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="151"/>
         <source>Ctrl+N</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="163"/>
         <source>Ctrl+O</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="175"/>
         <source>Ctrl+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="187"/>
         <source>Ctrl+Shift+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="199"/>
         <source>Ctrl+Q</source>
         <translation></translation>
     </message>
--- share/translations/app_pt_BR.ts
+++ share/translations/app_pt_BR.ts
@@ -4,82 +4,66 @@
 <context>
     <name>CircuitsModel</name>
     <message>
-        <location filename="../../src/circuitsTab.py" line="83"/>
         <source>Name</source>
         <translation>Nome</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="84"/>
         <source>Power</source>
         <translation>Potência</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="85"/>
         <source>Load Type</source>
         <translation>Tipo de Carga</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="86"/>
         <source>Voltage</source>
         <translation>Tensão</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="87"/>
         <source>Phases</source>
         <translation>Fases</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="88"/>
         <source>Grouping</source>
         <translation>Agrupamento</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="89"/>
         <source>Temperature</source>
         <translation>Temperatura</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="90"/>
         <source>Ref. Method</source>
         <translation>Método de Ref.</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="91"/>
         <source>Configuration</source>
         <translation>Configuração</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="92"/>
         <source>Wire Type</source>
         <translation>Tipo de Fio</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="93"/>
         <source>Length</source>
         <translation>Comprimento</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="94"/>
         <source>Current</source>
         <translation>Corrente</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="95"/>
         <source>Breaker</source>
         <translation>Disjuntor</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="96"/>
         <source>Wire Capacity</source>
         <translation>Capacidade do Fio</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="97"/>
         <source>Wire Section</source>
         <translation>Seção do Fio</translation>
     </message>
     <message>
-        <location filename="../../src/circuitsTab.py" line="201"/>
         <source>New Circuit</source>
         <translation>Novo Circuito</translation>
     </message>
@@ -87,97 +71,78 @@
 <context>
     <name>mainWindow</name>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="15"/>
         <source>NBR 5410 Calculator</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="26"/>
         <source>Panel</source>
         <translation>Painel</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="31"/>
         <source>Circuits</source>
         <translation>Circuitos</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="81"/>
         <source>New circuit</source>
         <translation>Novo circuito</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="92"/>
         <source>Delete circuit</source>
         <translation>Deletar circuito</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="104"/>
         <source>Conduits</source>
         <translation>Conduítes</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="123"/>
         <source>&amp;File</source>
         <translation>&amp;Arquivo</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="135"/>
         <source>&amp;Help</source>
         <translation>Aj&amp;uda</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="148"/>
         <source>&amp;New</source>
         <translation>&amp;Novo</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="160"/>
         <source>&amp;Open...</source>
         <translation>&amp;Abrir...</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="172"/>
         <source>&amp;Save</source>
         <translation>&amp;Salvar</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="184"/>
         <source>Save &amp;As...</source>
         <translation>Salvar &amp;Como...</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="196"/>
         <source>&amp;Quit</source>
         <translation>&amp;Sair</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="208"/>
         <source>&amp;About</source>
         <translation>&amp;Sobre</translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="151"/>
         <source>Ctrl+N</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="163"/>
         <source>Ctrl+O</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="175"/>
         <source>Ctrl+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="187"/>
         <source>Ctrl+Shift+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../src/ui/mainWindow.ui" line="199"/>
         <source>Ctrl+Q</source>
         <translation></translation>
     </message>
